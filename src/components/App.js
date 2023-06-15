e4179e50d5d216b10d9d29d52f3ffabaac2402b6
bonbongiraffe <francescowai@yahoo.com>
2023-06-15 11:26:24 -0400

    code-along

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import Header from "./Header";
 import Profile from "./Profile";
+import { UserProvider } from "../context/user";
 
 function App() {
   const [theme, setTheme] = useState("dark");
   const [user, setUser] = useState(null);
   return (
     <main className={theme}>
-      <Header theme={theme} setTheme={setTheme} user={user} setUser={setUser} />
-      <Profile theme={theme} user={user} />
+      {/* wrap components that need access to context data in the provider
+      You'll notice we also removed the `user` prop from these components, since we'll
+      be accessing that data via context instead.*/}
+      <UserProvider>
+        <Header theme={theme} setTheme={setTheme} />
+        <Profile theme={theme} />
+      </UserProvider>
     </main>
   );
 }
